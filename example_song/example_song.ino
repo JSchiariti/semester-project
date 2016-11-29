990a1ca98e6af2a9a5c82d0d85e49d23795537c1
JSchiariti <Schiaritij@rider.edu>
2016-11-29 11:16:31 -0500

    work in progress

---
+++ example_song/example_song.ino
@@ -0,0 +1,68 @@
+const int buzzerPin = 9;
+const int songLength = 18;
+char notes[] = "cdfda ag cdfdg gf ";
+int beats[] = {1,1,1,1,1,1,4,4,2,1,1,1,1,1,1,4,4,2};
+int tempo = 150;
+void setup() 
+{
+  pinMode(buzzerPin, OUTPUT);
+}
+
+void loop() 
+{
+  int m, duration;
+
+  for (m = 0; m < songLength; m++)
+  {
+    duration = beats[m] * tempo;
+
+    if (notes[m] == ' ')          
+    {
+      delay(duration);            
+    }
+    else                          
+    {
+      tone(buzzerPin, frequency(notes[m]), duration);
+      delay(duration);            
+    }
+    delay(tempo/10);              
+  }
+  
+  
+  while(true){}
+  
+}
+
+
+int frequency(char note) 
+{
+  
+  
+  int m;
+  const int numNotes = 8;  
+  
+ 
+
+  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+  int frequencies[] = {262, 294, 330, 349, 392, 440, 494, 523};
+  
+
+  
+  for (m = 0; m < numNotes; m++)  
+  {
+    if (names[m] == note)         
+    {
+      return(frequencies[m]);     
+    }
+  }
+  return(0);  
+              
+}
+
+  
+
+  
+
+  
+
+
